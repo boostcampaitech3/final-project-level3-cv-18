6e10ed8db4f9498d5b8231598ff53ff631724b99
kimkihoon0515 <tega1996@naver.com>
2022-06-07 09:40:01 +0000

    streamlit file upload method added

--- frontend/prototype.py
+++ frontend/prototype.py
@@ -2,11 +2,12 @@ import streamlit as st
 from PIL import Image
 import io
 import numpy as np
-import torch
 import cv2
 import pandas as pd
 import requests
-from backend.image_crop import crop
+from image_crop import crop
+import datetime
+from dateutil.tz import gettz
 
 
 import hashlib
@@ -26,7 +27,7 @@ def create_usertable():
 	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
 
 def create_userinfo():
-    c.execute('CREATE TABLE IF NOT EXISTS skin(username TEXT, wrinkle INT, oil INT, sensitive INT, pigmentation INT, hydration INT)')
+    c.execute('CREATE TABLE IF NOT EXISTS skin(username TEXT,date DATE, wrinkle INT, oil INT, sensitive INT, pigmentation INT, hydration INT)')
     conn.commit()
 
 
@@ -39,12 +40,12 @@ def login_user(username,password):
 	data = c.fetchall()
 	return data
 
-def add_skin_analysis(username,wrinkle,oil,sensitive,pigmentation,hydration):
-    c.execute('INSERT INTO skin(username,wrinkle,oil,sensitive,pigmentation,hydration) VALUES (?,?,?,?,?,?)',(username,wrinkle,oil,sensitive,pigmentation,hydration))
+def add_skin_analysis(username,date,wrinkle,oil,sensitive,pigmentation,hydration):
+    c.execute('INSERT INTO skin(username,date,wrinkle,oil,sensitive,pigmentation,hydration) VALUES (?,?,?,?,?,?,?)',(username,date,wrinkle,oil,sensitive,pigmentation,hydration))
     conn.commit()
 
 def view_skin_analysis(username):
-    s = f"SELECT wrinkle,oil,sensitive,pigmentation,hydration FROM SKIN WHERE USERNAME='{username}'"
+    s = f"SELECT date,wrinkle,oil,sensitive,pigmentation,hydration FROM SKIN WHERE USERNAME='{username}'"
     c.execute(s)
     data = c.fetchall()
     return data
@@ -63,7 +64,7 @@ choice = st.sidebar.selectbox("Menu",menu)
 
 
 if choice == "Login":
-    st.subheader("Login Section")
+    st.subheader("로그인 상태")
 
     username = st.sidebar.text_input("User Name")
     password = st.sidebar.text_input("Password",type='password')
@@ -76,50 +77,91 @@ if choice == "Login":
         result = login_user(username,check_hashes(password,hashed_pswd))
         if result:
 
-            st.success("Logged In as {}".format(username))
+            st.success("{}님 환영합니다.".format(username))
 
-            task = st.selectbox("Task",["Analytics","Profiles"])
+            task = st.selectbox("Task",["분석","기록"])
 
-            if task == "Analytics":
+            if task == "분석":
                 st.subheader('피부 평가')
-                img_file = st.camera_input('')
-                if img_file : 
-                    image_bytes = img_file.getvalue()
-                    img =Image.open(io.BytesIO(image_bytes))
-                    encoded_img = np.frombuffer(img_file.getvalue(),dtype=np.uint8)
-                    new_img = cv2.imdecode(encoded_img,cv2.IMREAD_COLOR)
-                    try:
-                        crop(new_img)
-                        crop_img = cv2.cvtColor(crop(new_img),cv2.COLOR_BGR2RGB)
-                        files = [
-                            ('files', (img_file.name, image_bytes,
-                                    img_file.type))
-                        ]
-                        cam_response = requests.post("http://101.101.217.13:30002/gradcam", files=files)
-                        cam_result = (np.array(cam_response.json()['cam']))
-                        label = cam_response.json()['label']
-                        col1,col2,col3,col4,col5 = st.columns(5)
-                        wrinkle,oil,sensitive,pigmentation,hydration=label[0],label[1],label[2],label[3],label[4]
-                        add_skin_analysis(username,wrinkle,oil,sensitive,pigmentation,hydration)
-                        c.close()
-                        with col1:
-                            st.image(cam_result[0],caption=f"label(wrinkle) : {wrinkle}")
-                        with col2:
-                            st.image(cam_result[1],caption=f"label(oil) : {oil}")
-                        with col3:
-                            st.image(cam_result[2],caption=f"label(sensitive) : {sensitive}")
-                        with col4:
-                            st.image(cam_result[3],caption=f"label(pigmentation) : {pigmentation}")
-                        with col5:
-                            st.image(cam_result[4],caption=f"label(hydration) : {hydration}")
-                    except:
-                        st.warning('사진을 다시 찍어주세요.')
-            elif task == "Profiles":
+                input_type = st.radio('평가 방식을 선택해주세요.',('파일 업로드','카메라'))
+                if input_type == '카메라':
+                    img_file = st.camera_input('')
+                    if img_file : 
+                        image_bytes = img_file.getvalue()
+                        img =Image.open(io.BytesIO(image_bytes))
+                        encoded_img = np.frombuffer(img_file.getvalue(),dtype=np.uint8)
+                        new_img = cv2.imdecode(encoded_img,cv2.IMREAD_COLOR)
+                        try:
+                            crop(new_img)
+                            crop_img = cv2.cvtColor(crop(new_img),cv2.COLOR_BGR2RGB)
+                            files = [
+                                ('files', (img_file.name, image_bytes,
+                                        img_file.type))
+                            ]
+                            with st.spinner(text='In progress'):
+                                cam_response = requests.post("http://101.101.217.13:30003/gradcam", files=files)
+                                cam_result = (np.array(cam_response.json()['cam']))
+                                label = cam_response.json()['label']
+                                col1,col2,col3,col4,col5 = st.columns(5)
+                                wrinkle,oil,sensitive,pigmentation,hydration=label[0],label[1],label[2],label[3],label[4]
+                                now = datetime.datetime.now(gettz('Asia/Seoul'))
+                                date = now.strftime("%Y-%m-%d-%H:%M")
+                                add_skin_analysis(username,date,wrinkle,oil,sensitive,pigmentation,hydration)
+                                c.close()
+                                with col1:
+                                    st.image(cam_result[0],caption=f"주름 : {wrinkle}")
+                                with col2:
+                                    st.image(cam_result[1],caption=f"유분 : {oil}")
+                                with col3:
+                                    st.image(cam_result[2],caption=f"민감도 : {sensitive}")
+                                with col4:
+                                    st.image(cam_result[3],caption=f"색조 : {pigmentation}")
+                                with col5:
+                                    st.image(cam_result[4],caption=f"수분 : {hydration}")
+                        except:
+                            st.warning('사진을 다시 찍어주세요.')
+                else:
+                    img_file = st.file_uploader('')
+                    if img_file : 
+                        image_bytes = img_file.getvalue()
+                        img =Image.open(io.BytesIO(image_bytes))
+                        encoded_img = np.frombuffer(img_file.getvalue(),dtype=np.uint8)
+                        new_img = cv2.imdecode(encoded_img,cv2.IMREAD_COLOR)
+                        try:
+                            crop(new_img)
+                            crop_img = cv2.cvtColor(crop(new_img),cv2.COLOR_BGR2RGB)
+                            files = [
+                                ('files', (img_file.name, image_bytes,
+                                        img_file.type))
+                            ]
+                            with st.spinner(text='In progress'):
+                                cam_response = requests.post("http://101.101.217.13:30003/gradcam", files=files)
+                                cam_result = (np.array(cam_response.json()['cam']))
+                                label = cam_response.json()['label']
+                                col1,col2,col3,col4,col5 = st.columns(5)
+                                wrinkle,oil,sensitive,pigmentation,hydration=label[0],label[1],label[2],label[3],label[4]
+                                now = datetime.datetime.now(gettz('Asia/Seoul'))
+                                date = now.strftime("%Y-%m-%d-%H:%M")
+                                add_skin_analysis(username,date,wrinkle,oil,sensitive,pigmentation,hydration)
+                                c.close()
+                                with col1:
+                                    st.image(cam_result[0],caption=f"주름 : {wrinkle}")
+                                with col2:
+                                    st.image(cam_result[1],caption=f"유분 : {oil}")
+                                with col3:
+                                    st.image(cam_result[2],caption=f"민감도 : {sensitive}")
+                                with col4:
+                                    st.image(cam_result[3],caption=f"색조 : {pigmentation}")
+                                with col5:
+                                    st.image(cam_result[4],caption=f"수분 : {hydration}")
+                        except:
+                            st.warning('사진을 다시 찍어주세요.')
+            elif task == "기록":
                 st.subheader("평가 기록")
                 st.write('0 : 매우적음, 4 : 매우 많음')
                 user_result = view_skin_analysis(username)
                 c.close()
-                clean_db = pd.DataFrame(user_result,columns=['wrinkle','oil','sensative','pigmentation','hydration'])
+                clean_db = pd.DataFrame(user_result,columns=['날짜','주름','유분','민감도','색조','수분']).set_index('날짜')
                 st.dataframe(clean_db)
                 with st.expander("그래프로 확인하기"):
                     st.line_chart(clean_db)
@@ -144,7 +186,7 @@ elif choice == "SignUp":
             add_userdata(new_user,make_hashes(new_password))
             c.close()
             st.success("계정 생성에 성공하셨습니다.")
-            st.info("Go to Login Menu to login")
+            st.info("왼쪽 로그인 메뉴를 통해 로그인해주세요.")
 
 
 
