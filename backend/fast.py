c4e3e94d56728a21ae2dc6363e59664d3000373e
kimkihoon0515 <tega1996@naver.com>
2022-06-10 04:47:22 +0000

    backend server seperated & streamlit code updated

---
+++ backend/fast.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, File, UploadFile
+from prediction import predict
+import uvicorn
+import numpy as np
+from typing import List
+import cv2
+from image_crop import crop
+from grad_cam import GradCam,show_cam_on_image,preprocess_image
+from efficientnet_pytorch import EfficientNet
+from fastapi import FastAPI
+import torch
+import time
+from starlette.responses import Response
+
+app = FastAPI()
+
+
+@app.post('/gradcam')
+async def get_gradcam(files: List[UploadFile] = File(...)):
+    global model
+    image_bytes = await files[0].read()
+    encoded_img = np.frombuffer(image_bytes,dtype=np.uint8)
+    new_img = cv2.imdecode(encoded_img,cv2.IMREAD_COLOR)
+
+    crop_img = crop(new_img)
+    start= time.time()
+    label = predict(model=model,img=crop_img)
+
+    rgb_img=cv2.cvtColor(crop_img, cv2.COLOR_RGB2BGR)
+    rgb_img = np.float32(rgb_img) / 255
+
+    cam_img = preprocess_image(rgb_img)
+    # target layer
+    target_layer_names = [str(len(model._blocks) - 1)]
+    img_size = (rgb_img.shape[1], rgb_img.shape[0])
+    grad_cam = GradCam(model=model, blob_name = '_blocks', target_layer_names=target_layer_names, use_cuda=True,img_size=img_size)
+    mask_dic = grad_cam(cam_img, label)
+
+    for k,v in mask_dic.items():
+        gray_mask=v
+        name=k
+
+    cam_image = show_cam_on_image(rgb_img, 1-gray_mask, name)
+    cam_list = cam_image.tolist()
+    #cam_list.append(cam_image.tolist())
+    end = time.time()
+    print(end-start)
+    return {'cam':cam_list,'label':label}
+
+
+
+
+def main():
+    uvicorn.run(app, host="172.17.0.2", port=30002)
+
+if __name__ == '__main__':
+    model_path = '/opt/ml/input/artlab/models/wrinkle_best_precall.pth'
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = EfficientNet.from_pretrained('efficientnet-b0',num_classes=5).to(device)
+    model.load_state_dict(torch.load(model_path,map_location=device))
+    main()
--- frontend/prototype.py
+++ frontend/prototype.py
@@ -5,9 +5,19 @@ import numpy as np
 import cv2
 import pandas as pd
 import requests
-from image_crop import crop
+from backend.image_crop import crop
 import datetime
 from dateutil.tz import gettz
+import time
+from streamlit_option_menu import option_menu
+from concurrent.futures import ThreadPoolExecutor
+
+
+st.set_page_config(page_title = 'ARTLAB 피부평가',layout="wide")
+
+def post_url(args):
+    return requests.post(args[0],files=args[1])
+
 
 
 import hashlib
@@ -56,25 +66,158 @@ def check_user(username):
     data = c.fetchone()
     return data
 
-st.title("XAI 피부평가 Demo버전")
+def image_upload():
+    img_file = st.file_uploader('')
+    if img_file : 
+        image_bytes = img_file.getvalue()
+        img =Image.open(io.BytesIO(image_bytes))
+        encoded_img = np.frombuffer(img_file.getvalue(),dtype=np.uint8)
+        new_img = cv2.imdecode(encoded_img,cv2.IMREAD_COLOR)
+        try:
+            crop(new_img)
+            crop_img = cv2.cvtColor(crop(new_img),cv2.COLOR_BGR2RGB)
+            files = [
+                ('files', (img_file.name, image_bytes,
+                        img_file.type))
+            ]
+            start = time.time()
+            with st.spinner(text='In progress'):
+                list_of_urls = [('http://101.101.217.13:30002/gradcam',files),('http://101.101.217.13:30004/gradcam',files),('http://115.85.183.56:30001/gradcam',files),('http://115.85.183.56:30002/gradcam',files),('http://115.85.183.56:30003/gradcam',files)]
+                with ThreadPoolExecutor(max_workers=5) as pool:
+                    response_list = list(pool.map(post_url,list_of_urls))
+                wrinkle_cam_response = response_list[0] # 주름
+                oil_cam_response = response_list[1] # 유분
+                sensitive_cam_response = response_list[2] # 민감도
+                pig_cam_response = response_list[3] # 색조
+                hyd_cam_response = response_list[4] # 수분
 
-global crop_img
-menu = ["Login","SignUp"]
-choice = st.sidebar.selectbox("Menu",menu)
+                wrinkle_result = (np.array(wrinkle_cam_response.json()['cam'])) # 주름
+                wrinkle_label = wrinkle_cam_response.json()['label'] # 주름
+
+                oil_result = (np.array(oil_cam_response.json()['cam'])) # 유분
+                oil_label = oil_cam_response.json()['label'] # 유분
+                
+    
+                sensitive_result = (np.array(sensitive_cam_response.json()['cam']))
+                sensitive_label = sensitive_cam_response.json()['label']
+
+                pig_result = (np.array(pig_cam_response.json()['cam']))
+                pig_label = pig_cam_response.json()['label']
+
+                hyd_result = (np.array(hyd_cam_response.json()['cam']))
+                hyd_label = hyd_cam_response.json()['label']
+
+                col1,col2,col3,col4,col5 = st.columns(5)
+                wrinkle,oil,sensitive,pigmentation,hydration=wrinkle_label,oil_label,sensitive_label,pig_label,hyd_label
+                now = datetime.datetime.now(gettz('Asia/Seoul'))
+                date = now.strftime("%Y-%m-%d-%H:%M")
+                add_skin_analysis(username,date,wrinkle,oil,sensitive,pigmentation,hydration)
+                c.close()
+                with col1:
+                    st.image(wrinkle_result,caption=f"주름 : {wrinkle}")
+                with col2:
+                    st.image(oil_result,caption=f"유분 : {oil}")
+                with col3:
+                    st.image(sensitive_result,caption=f"민감도 : {sensitive}")
+                with col4:
+                    st.image(pig_result,caption=f"색조 : {pigmentation}")
+                with col5:
+                    st.image(hyd_result,caption=f"수분 : {hydration}")
+                end = time.time()
+                st.write(end-start)
+        except:
+            st.warning('사진을 다시 찍어주세요.')
+
+def camera_input():
+    img_file = st.camera_input('')
+    if img_file : 
+        image_bytes = img_file.getvalue()
+        img =Image.open(io.BytesIO(image_bytes))
+        encoded_img = np.frombuffer(img_file.getvalue(),dtype=np.uint8)
+        new_img = cv2.imdecode(encoded_img,cv2.IMREAD_COLOR)
+        try:
+            crop(new_img)
+            crop_img = cv2.cvtColor(crop(new_img),cv2.COLOR_BGR2RGB)
+            files = [
+                ('files', (img_file.name, image_bytes,
+                        img_file.type))
+            ]
+            start = time.time()
+            with st.spinner(text='In progress'):
+                list_of_urls = [('http://101.101.217.13:30002/gradcam',files),('http://101.101.217.13:30004/gradcam',files),('http://115.85.183.56:30001/gradcam',files),('http://115.85.183.56:30002/gradcam',files),('http://115.85.183.56:30003/gradcam',files)]
+                with ThreadPoolExecutor(max_workers=5) as pool:
+                    response_list = list(pool.map(post_url,list_of_urls))
+                wrinkle_cam_response = response_list[0] # 주름
+                oil_cam_response = response_list[1] # 유분
+                sensitive_cam_response = response_list[2] # 민감도
+                pig_cam_response = response_list[3] # 색조
+                hyd_cam_response = response_list[4] # 수분
 
 
-if choice == "Login":
+                wrinkle_result = (np.array(wrinkle_cam_response.json()['cam'])) # 주름
+                wrinkle_label = wrinkle_cam_response.json()['label'] # 주름
+
+                oil_result = (np.array(oil_cam_response.json()['cam'])) # 유분
+                oil_label = oil_cam_response.json()['label'] # 유분
+                
+
+                sensitive_result = (np.array(sensitive_cam_response.json()['cam']))
+                sensitive_label = sensitive_cam_response.json()['label']
+
+                pig_result = (np.array(pig_cam_response.json()['cam']))
+                pig_label = pig_cam_response.json()['label']
+
+                hyd_result = (np.array(hyd_cam_response.json()['cam']))
+                hyd_label = hyd_cam_response.json()['label']
+
+                col1,col2,col3,col4,col5 = st.columns(5)
+                wrinkle,oil,sensitive,pigmentation,hydration=wrinkle_label,oil_label,sensitive_label,pig_label,hyd_label
+                now = datetime.datetime.now(gettz('Asia/Seoul'))
+                date = now.strftime("%Y-%m-%d-%H:%M")
+                add_skin_analysis(username,date,wrinkle,oil,sensitive,pigmentation,hydration)
+                c.close()
+                with col1:
+                    st.image(wrinkle_result,caption=f"주름 : {wrinkle}")
+                with col2:
+                    st.image(oil_result,caption=f"유분 : {oil}")
+                with col3:
+                    st.image(sensitive_result,caption=f"민감도 : {sensitive}")
+                with col4:
+                    st.image(pig_result,caption=f"색조 : {pigmentation}")
+                with col5:
+                    st.image(hyd_result,caption=f"수분 : {hydration}")
+                end = time.time()
+                st.write(end-start)
+        except:
+            st.warning('사진을 다시 찍어주세요.')
+
+
+
+
+st.sidebar.image('/opt/ml/input/artlab/backend/logo/artlab.png')
+with st.sidebar:
+    choice = option_menu('Main menu',['Home','로그인','회원가입'],
+    icons = ['box-arrow-in-left','gear'],menu_icon='menu-button',default_index=0)
+    choice
+
+
+if choice == "로그인":
     st.subheader("로그인 상태")
 
     username = st.sidebar.text_input("User Name")
     password = st.sidebar.text_input("Password",type='password')
     if st.sidebar.checkbox("Login"):
-        # if password == '12345':
         create_usertable()
         create_userinfo()
         hashed_pswd = make_hashes(password)
 
         result = login_user(username,check_hashes(password,hashed_pswd))
+
+        if 'name' not in st.session_state:
+            st.session_state.name = None
+        else:
+            st.session_state.name = username
+
         if result:
 
             st.success("{}님 환영합니다.".format(username))
@@ -83,81 +226,21 @@ if choice == "Login":
 
             if task == "분석":
                 st.subheader('피부 평가')
-                input_type = st.radio('평가 방식을 선택해주세요.',('파일 업로드','카메라'))
-                if input_type == '카메라':
-                    img_file = st.camera_input('')
-                    if img_file : 
-                        image_bytes = img_file.getvalue()
-                        img =Image.open(io.BytesIO(image_bytes))
-                        encoded_img = np.frombuffer(img_file.getvalue(),dtype=np.uint8)
-                        new_img = cv2.imdecode(encoded_img,cv2.IMREAD_COLOR)
-                        try:
-                            crop(new_img)
-                            crop_img = cv2.cvtColor(crop(new_img),cv2.COLOR_BGR2RGB)
-                            files = [
-                                ('files', (img_file.name, image_bytes,
-                                        img_file.type))
-                            ]
-                            with st.spinner(text='In progress'):
-                                cam_response = requests.post("http://101.101.217.13:30003/gradcam", files=files)
-                                cam_result = (np.array(cam_response.json()['cam']))
-                                label = cam_response.json()['label']
-                                col1,col2,col3,col4,col5 = st.columns(5)
-                                wrinkle,oil,sensitive,pigmentation,hydration=label[0],label[1],label[2],label[3],label[4]
-                                now = datetime.datetime.now(gettz('Asia/Seoul'))
-                                date = now.strftime("%Y-%m-%d-%H:%M")
-                                add_skin_analysis(username,date,wrinkle,oil,sensitive,pigmentation,hydration)
-                                c.close()
-                                with col1:
-                                    st.image(cam_result[0],caption=f"주름 : {wrinkle}")
-                                with col2:
-                                    st.image(cam_result[1],caption=f"유분 : {oil}")
-                                with col3:
-                                    st.image(cam_result[2],caption=f"민감도 : {sensitive}")
-                                with col4:
-                                    st.image(cam_result[3],caption=f"색조 : {pigmentation}")
-                                with col5:
-                                    st.image(cam_result[4],caption=f"수분 : {hydration}")
-                        except:
-                            st.warning('사진을 다시 찍어주세요.')
+                with st.sidebar:
+                    select_method = option_menu('분석방법',['사진 촬영','저장된 사진 업로드'],
+                    icons = ['camera-fill','upload'],menu_icon='menu-button',default_index=0)
+                    select_method
+                if select_method == '사진 촬영':
+                    st.markdown('### 사진을 촬영하여 피부를 검사하는 방법입니다.')
+                    camera_input()
                 else:
-                    img_file = st.file_uploader('')
-                    if img_file : 
-                        image_bytes = img_file.getvalue()
-                        img =Image.open(io.BytesIO(image_bytes))
-                        encoded_img = np.frombuffer(img_file.getvalue(),dtype=np.uint8)
-                        new_img = cv2.imdecode(encoded_img,cv2.IMREAD_COLOR)
-                        try:
-                            crop(new_img)
-                            crop_img = cv2.cvtColor(crop(new_img),cv2.COLOR_BGR2RGB)
-                            files = [
-                                ('files', (img_file.name, image_bytes,
-                                        img_file.type))
-                            ]
-                            with st.spinner(text='In progress'):
-                                cam_response = requests.post("http://101.101.217.13:30003/gradcam", files=files)
-                                cam_result = (np.array(cam_response.json()['cam']))
-                                label = cam_response.json()['label']
-                                col1,col2,col3,col4,col5 = st.columns(5)
-                                wrinkle,oil,sensitive,pigmentation,hydration=label[0],label[1],label[2],label[3],label[4]
-                                now = datetime.datetime.now(gettz('Asia/Seoul'))
-                                date = now.strftime("%Y-%m-%d-%H:%M")
-                                add_skin_analysis(username,date,wrinkle,oil,sensitive,pigmentation,hydration)
-                                c.close()
-                                with col1:
-                                    st.image(cam_result[0],caption=f"주름 : {wrinkle}")
-                                with col2:
-                                    st.image(cam_result[1],caption=f"유분 : {oil}")
-                                with col3:
-                                    st.image(cam_result[2],caption=f"민감도 : {sensitive}")
-                                with col4:
-                                    st.image(cam_result[3],caption=f"색조 : {pigmentation}")
-                                with col5:
-                                    st.image(cam_result[4],caption=f"수분 : {hydration}")
-                        except:
-                            st.warning('사진을 다시 찍어주세요.')
+                    st.markdown('### 저장된사진을 통해 피부를 검사하는 방법입니다.')
+                    st.markdown('#### 조금 더 정밀한 결과를 받아보실 수 있습니다.')
+                    image_upload()
+
             elif task == "기록":
                 st.subheader("평가 기록")
+                st.markdown("## 사용자의 피부 진단 기록을 확인할 수 있는 공간입니다.")
                 st.write('0 : 매우적음, 4 : 매우 많음')
                 user_result = view_skin_analysis(username)
                 c.close()
@@ -165,13 +248,10 @@ if choice == "Login":
                 st.dataframe(clean_db)
                 with st.expander("그래프로 확인하기"):
                     st.line_chart(clean_db)
-                #user_result = view_all_users()
-                #clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-                #st.dataframe(clean_db)
         else:
             st.warning("아이디 또는 비밀번호가 맞지 않습니다.")
 
-elif choice == "SignUp":
+elif choice == "회원가입":
     st.subheader("Create New Account")
     new_user = st.text_input("Username")
     new_password = st.text_input("Password",type='password')
@@ -186,10 +266,15 @@ elif choice == "SignUp":
             add_userdata(new_user,make_hashes(new_password))
             c.close()
             st.success("계정 생성에 성공하셨습니다.")
-            st.balloons()
             st.info("왼쪽 로그인 메뉴를 통해 로그인해주세요.")
 
-
+elif choice == 'Home':
+    st.image('/opt/ml/input/artlab/backend/logo/h_logo.png',use_column_width='always')
+    st.title('ARTLAB 기업연계 프로젝트입니다.')
+    st.markdown('### 사진을 찍거나 업로드하여 5개의 항목에 대해 평가하는 서비스입니다.')
+    st.image('/opt/ml/input/artlab/backend/logo/example.png',use_column_width='always')
+    st.markdown('### 사진을 찍거나 업로드하시면 각각의 항목에 대해 점수와 함께 grad-cam 결과를 얻습니다.')
+    st.markdown('### 또한 저희 데모버전은 사용자 별 진단 기록을 저장하여 시각화하는 과정을 포함하고 있습니다.')
 
 
 
