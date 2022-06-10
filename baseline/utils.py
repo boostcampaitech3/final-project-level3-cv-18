89b17e80a1a5e28694c25d2f30374ab78c29fed5
ysw2946 <ysw2946@naver.com>
2022-06-10 08:16:39 +0000

    add EDA

--- baseline/utils.py
+++ baseline/utils.py
@@ -1,3 +1,11 @@
+import os
+import json
+
+from tqdm import tqdm
+from glob import glob
+import shutil
+
+import cv2
 import torch
 import torch.nn.functional as F
 
@@ -88,3 +96,58 @@ def MSE(outputs, labels):
     mse *= mse
 
     return torch.mean(mse)
+
+class LipsCrop:
+    def make_crop_image(data_path,save_path):
+        if not os.path.exists(save_path):
+                os.makedirs(save_path)
+
+        # 턱 이미지만 골라오기
+        chin_image_path = []
+
+        annotation_paths = glob(os.path.join(data_path,'*.json'))
+        for path in annotation_paths:
+            with open(path) as f:
+                anno = json.load(f)
+
+                # 턱 이미지가 아니라면 무시
+                if anno['part'] != 3:
+                    continue
+            
+                chin_image_path.append(os.path.join(data_path,anno['file_name']))
+                shutil.copyfile(path, os.path.join(save_path, '1' + anno['file_name'].split('.')[0]+'.json'))
+
+        chin_image_path.sort()
+        
+        # 입술을 검출해주는 모델 생성
+        mouth_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+
+        # 턱 사진에서 입술만 크롭하여 저장하기
+        for path in tqdm(chin_image_path):
+            img = cv2.imread(path)
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            file_name = '1' + path.split('/')[-1]
+
+            # 입술 ROI 영역 생성
+            mouths = mouth_cascade.detectMultiScale(gray,1.1,20)
+
+            # 후보 영역이 여러개인 경우
+            mouth_list = []
+            if len(mouths) != 1:
+                for (x,y,w,h) in mouths:
+                    mouth_list.append(w)
+                    
+                if len(mouth_list) == 0:
+                    continue
+
+                else:
+                    idx = mouth_list.index(max(mouth_list))
+                    mouth = mouths[idx]
+            
+            else:
+                mouth = mouths[0]
+            
+            x,y,w,h = mouth
+            
+            crop_image = img[y:y+h,x:x+w]
+            cv2.imwrite(os.path.join(save_path,file_name),crop_image)
